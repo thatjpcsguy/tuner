8d7b42a5fb78da8aa1d1e3b0c1e70224e3abac9b
James Cooper-Stanbury <james@moonblade.lan>
2017-04-06 09:17:32 +1000

    more simplification

--- app.py
+++ app.py
@@ -27,9 +27,9 @@ config.read('tuner.conf')
 
 episode_info_regex = re.compile(ur'(S?([0-9]{1,2})[x|E]([0-9]{1,2}))', re.IGNORECASE)
 episode_id_regex = re.compile(ur'/ep/([0-9]*)/', re.IGNORECASE)
-episode_magnet_regex = re.compile(ur'href="(.*)" class', re.IGNORECASE)
+episode_magnet_regex = re.compile(ur' href="(.*)" rel', re.IGNORECASE)
 
-transmission_server = "http://" + config.get("transmission", "host") + "/ui/rpc"
+transmission_server = "https://" + config.get("transmission", "host") + "/ui/rpc"
 
 db = None
 
@@ -67,11 +67,9 @@ def update_available_shows():
         for font in row.find_all('font'):
             status = font.get_text()
 
-        #print info
         try:
             db.query("INSERT INTO shows (`show_id`, `name`, `url`, `path`, `status`, `download`) VALUES ('%s', '%s', '%s', '%s', '%s', 0) ON DUPLICATE KEY UPDATE status = '%s', url = '%s'" % (show_id, db.escape_string(str(name)), url, path, db.escape_string(status), db.escape_string(status), url))
         except:
-            #print "Error inserting show"
             pass
 
 def update_available_eps(url, show_id):
@@ -80,26 +78,22 @@ def update_available_eps(url, show_id):
     eps = BeautifulSoup(ep.text, "lxml")
 
     for episode in eps.find_all('tr'):
-        try:
-            ep_string = str(episode.find('a', class_="epinfo"))
-            info = re.findall(episode_info_regex, ep_string)
-            if len(info) > 0:
-                season = info[0][1]
-                number = info[0][2]
-            else:
-                continue
+        ep_string = str(episode.find('a', class_="epinfo"))
+        info = re.findall(episode_info_regex, ep_string)
+        if len(info) > 0:
+            season = info[0][1]
+            number = info[0][2]
+        else:
+            continue
 
-            ep_id = re.findall(episode_id_regex, ep_string)
-            if len(ep_id) > 0:
-                episode_id = ep_id[0] 
+        ep_id = re.findall(episode_id_regex, ep_string)
+        if len(ep_id) > 0:
+            episode_id = ep_id[0] 
 
-            magnet = episode.find('a', class_="magnet")
+        magnet = episode.find('a', class_="magnet")
+        if magnet is not None:
             magnet = db.escape_string(re.findall(episode_magnet_regex, str(magnet))[0])
-
             db.query("INSERT INTO episodes (`show_id`, `episode_id`, `number`, `season`, `magnet`, `downloaded`) VALUES ('%s', '%s', '%s', '%s', '%s', 0) ON DUPLICATE KEY UPDATE magnet = '%s'" % (show_id, episode_id, number, season, magnet, magnet))
-        except:
-            #print "unable to insert episode"
-            pass
 
 
 def check_new_eps_active():
@@ -119,7 +113,7 @@ def download_missing():
 
     headers = {'X-Transmission-Session-Id': session}
 
-    db.query("SELECT s.show_id, season, number, magnet, s.name show_name FROM episodes e JOIN shows s ON e.show_id = s.show_id  WHERE `download` = 1 GROUP BY s.show_id, season, number HAVING MAX(downloaded) = 0")
+    db.query("SELECT s.show_id, season, number, path, MIN(magnet) magnet, s.name show_name FROM episodes e JOIN shows s ON e.show_id = s.show_id  WHERE `download` = 1 GROUP BY s.show_id, season, number, s.name, path HAVING MAX(downloaded) = 0")
     res = db.store_result()
     i = res.fetch_row(how=1)
     
@@ -134,31 +128,11 @@ def download_missing():
                     "download-dir": config.get("transmission", "dir") + "/%s/season %s/" % (i[0]['path'].replace("-", " "), i[0]['season'])
                 }
             }
-        #print payload
         r = requests.post(transmission_server, data=json.dumps(payload), headers=headers)
         if r.status_code == 200:
             db.query("UPDATE episodes SET downloaded = 1 WHERE show_id = %s AND number = %s and season = %s" % (i[0]['show_id'], i[0]['number'], i[0]['season']))
 
         i = res.fetch_row(how=1)
-
-    if downloading:
-        print downloading
-        # for number in config.get("twilio", "to").split(","):
-
-            # send_sms(number.strip(), downloading)
-
-
-# def send_sms(to, body):
-#     try:
-#         twilio.messages.create(
-#                 to=to, 
-#                 from_=config.get("twilio", "from"),
-#                 body=body
-#             )
-#     except:
-#         print "could not send sms"
-#         pass
-
 
 
 if __name__ == '__main__':
